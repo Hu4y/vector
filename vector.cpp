7d6a9fca54ce8f79d8685ddc25b841828742f445
Hu4y <xzsawq0610@gmail.com>
2025-09-04 00:04:59 +0800

    Update vector.cpp

--- vector.cpp
+++ vector.cpp
@@ -1,1 +1,23 @@
-﻿
+﻿#include <iostream>
+#include <vector>
+
+int main() {
+   int n, q;
+   std::cin >> n >> q;
+
+   std::vector<std::vector<int>> vec(n);
+
+   for(int i = 0; i < n; i++) {
+      int k;
+      std::cin >> k;
+      vec[i].resize(k);
+      for(int j = 0; j < k; j++) {
+         std::cin >> vec[i][j];
+      }
+   }
+   for(int t = 0; t < q; t++) {
+      int a, b;
+      std::cin >> a >> b;
+      std::cout << vec[a][b] << std::endl;
+      }
+}
